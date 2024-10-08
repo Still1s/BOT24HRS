40b01c8c2d12008a452ceeddd24049a775e200e8
Still1s <meliodas28272@gmail.com>
2024-10-08 13:24:42 -0300

    index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'Craft24hrs.aternos.me',
     version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-    username: 'AFK24HrS',
+    username: 'AFK24HRS',
     port: 41262,
     plugins: [AutoAuth],
     AutoAuth: 'bot1122033'
